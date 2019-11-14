f47ba99ca269a737d4aac840b0a7011bd1288d68
Matheus Pedroni <matheusr@ciandt.com>
2019-11-13 21:08:11 -0300

    Store list

--- services/api.service.js
+++ services/api.service.js
@@ -40,6 +40,8 @@ const ApiService = {
         return userData != null;
     },
 
+    
+
     parseJwt (token) {
         var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
